899e9ba7b03bd8d1e7f27f4f8aeea2e802daaa0c
Peralta Rocio <Peralta.rocio.2229@gmail.com>
2025-09-10 00:41:41 -0300

    ejercicio 2

---
+++ 2.js
@@ -0,0 +1,19 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+// Ejercicio 2: Do While
+
+// Escribe un programa que solicite al usuario ingresar una contraseña. 
+// Si la contraseña es incorrecta, debe volver a pedirla hasta que el usuario ingrese la correcta.
+//  La contraseña correcta es "1234".
+
+// let contraseña;
+// const claveCorrecta ="1234";
+
+// do{
+//      contraseña = prompt ("ingrese una contraseña de 4 digitos");
+//   if (contraseña !== claveCorrecta ){
+//     alert ("Acceso Denegado")
+//   } else {
+//     alert ("acceso concedido")
+//   }
+//     } while (contraseña !== claveCorrecta);
