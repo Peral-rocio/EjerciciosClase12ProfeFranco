3b65d7191b08f79aed3ccca39b10c95876fdce95
Peralta Rocio <Peralta.rocio.2229@gmail.com>
2025-09-09 21:10:31 -0300

    Ejercicio 1

---
+++ 1.js
@@ -0,0 +1,27 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+// Ejercicio 1: While
+
+// Crea un programa que solicite al usuario ingresar números continuamente 
+//  hasta que el usuario ingrese un número negativo.
+//  Luego, imprime la suma 
+// de todos los números ingresados.
+
+//SOLUCION 
+
+// let suma = 0
+// let numero = 0
+
+// while (true) {
+//     numero = parseInt (prompt ("ingrese un numero porfavor"));
+
+//     if (isNaN (numero) || numero < 0 ){
+//     alert ("Valores Invalidos")
+//     break;
+//     } else {
+//       console.log (`${suma += numero}`)
+//     }
+// }
+
+
+
